348afd9ca04ec8959f9c71944bd5adff60b3675f
Rocio-anabel <163307652+Rocio-anabel@users.noreply.github.com>
2024-09-25 20:44:49 -0300

    Add cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
 const translate = require('node-google-translate-skidz');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.get('/', (req, res)=> {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
